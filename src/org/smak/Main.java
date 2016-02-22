729adfdf5ade43075a92a088730d7cdf740509a6
Antony <blokant@gmail.com>
2016-02-22 23:26:20 +0300

    Added helper routine: set series for each number from left to right

--- src/org/smak/Main.java
+++ src/org/smak/Main.java
@@ -5,6 +5,6 @@ public class Main {
     public static void main(String[] args) {
 
         SliceMatcher sliceMatcher = new SliceMatcher();
-        System.out.println(sliceMatcher.getSeries(56));
+        System.out.println(sliceMatcher.getSeries(12000));
     }
 }
--- src/org/smak/SliceMatcher.java
+++ src/org/smak/SliceMatcher.java
@@ -7,16 +7,23 @@ import java.util.Map;
  * Created by smak on 22.02.16.
  */
 public class SliceMatcher {
+    private final int sliceLength;
     private Map<Integer, String> data;
     public String getSeries(int number){
         return data.get(number);
     }
+    public void setForSlice(int left, int right, String series){
+        for (int i = left; i < right; i++) {
+            data.put(i,series);
+        }
+    }
     public SliceMatcher(){
+        sliceLength = 1000;
         data = new HashMap<Integer, String>();
-        for (int i = 0; i < 1000; i++) {
+        for (int i = 0; i < sliceLength; i++) {
             data.put(i,"AA");
-            data.put(i+1000,"AB");
-            data.put(i+2000,"AC");
+            data.put(i+sliceLength,"AB");
+            data.put(i+sliceLength*2,"AC");
         }
     }
 }
